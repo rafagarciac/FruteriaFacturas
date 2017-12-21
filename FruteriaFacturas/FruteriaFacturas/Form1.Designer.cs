cef50fff462093b21fc4337fe2b02a3c338dcdce
rafagarciac <noviembre98>
2017-12-21 23:36:48 +0100

    Muestro el Formulario Cliente y Controlo que no este repetido con el metodo: formRepetido(Form) reutilizable para los demas formularios

--- FruteriaFacturas/FruteriaFacturas/Form1.Designer.cs
+++ FruteriaFacturas/FruteriaFacturas/Form1.Designer.cs
@@ -31,23 +31,23 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.creacionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.modificacionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.borradoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.consultaDatosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.informesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.pbfondoMDI = new System.Windows.Forms.PictureBox();
-            this.albaranToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clientesToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.facturaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.albaranToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modificacionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clientesToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
             this.albaranToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.facturaToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.borradoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clientesToolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
             this.albaranToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
             this.facturaToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultaDatosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.clientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.albaranesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.facturasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.clientesToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.clientesToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
-            this.clientesToolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.informesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pbfondoMDI = new System.Windows.Forms.PictureBox();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbfondoMDI)).BeginInit();
             this.SuspendLayout();
@@ -76,6 +76,25 @@
             this.creacionToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
             this.creacionToolStripMenuItem.Text = "Creacion";
             // 
+            // clientesToolStripMenuItem1
+            // 
+            this.clientesToolStripMenuItem1.Name = "clientesToolStripMenuItem1";
+            this.clientesToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.clientesToolStripMenuItem1.Text = "Clientes";
+            this.clientesToolStripMenuItem1.Click += new System.EventHandler(this.clientesToolStripMenuItem1_Click);
+            // 
+            // facturaToolStripMenuItem
+            // 
+            this.facturaToolStripMenuItem.Name = "facturaToolStripMenuItem";
+            this.facturaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.facturaToolStripMenuItem.Text = "Factura";
+            // 
+            // albaranToolStripMenuItem
+            // 
+            this.albaranToolStripMenuItem.Name = "albaranToolStripMenuItem";
+            this.albaranToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.albaranToolStripMenuItem.Text = "Albaran";
+            // 
             // modificacionToolStripMenuItem
             // 
             this.modificacionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -85,6 +104,24 @@
             this.modificacionToolStripMenuItem.Name = "modificacionToolStripMenuItem";
             this.modificacionToolStripMenuItem.Size = new System.Drawing.Size(89, 20);
             this.modificacionToolStripMenuItem.Text = "Modificacion";
+            // 
+            // clientesToolStripMenuItem2
+            // 
+            this.clientesToolStripMenuItem2.Name = "clientesToolStripMenuItem2";
+            this.clientesToolStripMenuItem2.Size = new System.Drawing.Size(116, 22);
+            this.clientesToolStripMenuItem2.Text = "Clientes";
+            // 
+            // albaranToolStripMenuItem1
+            // 
+            this.albaranToolStripMenuItem1.Name = "albaranToolStripMenuItem1";
+            this.albaranToolStripMenuItem1.Size = new System.Drawing.Size(116, 22);
+            this.albaranToolStripMenuItem1.Text = "Albaran";
+            // 
+            // facturaToolStripMenuItem1
+            // 
+            this.facturaToolStripMenuItem1.Name = "facturaToolStripMenuItem1";
+            this.facturaToolStripMenuItem1.Size = new System.Drawing.Size(116, 22);
+            this.facturaToolStripMenuItem1.Text = "Factura";
             // 
             // borradoToolStripMenuItem
             // 
@@ -96,6 +133,24 @@
             this.borradoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
             this.borradoToolStripMenuItem.Text = "Borrado";
             // 
+            // clientesToolStripMenuItem3
+            // 
+            this.clientesToolStripMenuItem3.Name = "clientesToolStripMenuItem3";
+            this.clientesToolStripMenuItem3.Size = new System.Drawing.Size(116, 22);
+            this.clientesToolStripMenuItem3.Text = "Clientes";
+            // 
+            // albaranToolStripMenuItem2
+            // 
+            this.albaranToolStripMenuItem2.Name = "albaranToolStripMenuItem2";
+            this.albaranToolStripMenuItem2.Size = new System.Drawing.Size(116, 22);
+            this.albaranToolStripMenuItem2.Text = "Albaran";
+            // 
+            // facturaToolStripMenuItem2
+            // 
+            this.facturaToolStripMenuItem2.Name = "facturaToolStripMenuItem2";
+            this.facturaToolStripMenuItem2.Size = new System.Drawing.Size(116, 22);
+            this.facturaToolStripMenuItem2.Text = "Factura";
+            // 
             // consultaDatosToolStripMenuItem
             // 
             this.consultaDatosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -105,6 +160,24 @@
             this.consultaDatosToolStripMenuItem.Name = "consultaDatosToolStripMenuItem";
             this.consultaDatosToolStripMenuItem.Size = new System.Drawing.Size(99, 20);
             this.consultaDatosToolStripMenuItem.Text = "Consulta Datos";
+            // 
+            // clientesToolStripMenuItem
+            // 
+            this.clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
+            this.clientesToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.clientesToolStripMenuItem.Text = "Clientes";
+            // 
+            // albaranesToolStripMenuItem
+            // 
+            this.albaranesToolStripMenuItem.Name = "albaranesToolStripMenuItem";
+            this.albaranesToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.albaranesToolStripMenuItem.Text = "Albaranes";
+            // 
+            // facturasToolStripMenuItem
+            // 
+            this.facturasToolStripMenuItem.Name = "facturasToolStripMenuItem";
+            this.facturasToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.facturasToolStripMenuItem.Text = "Facturas";
             // 
             // informesToolStripMenuItem
             // 
@@ -121,78 +194,6 @@
             this.pbfondoMDI.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbfondoMDI.TabIndex = 3;
             this.pbfondoMDI.TabStop = false;
-            // 
-            // albaranToolStripMenuItem
-            // 
-            this.albaranToolStripMenuItem.Name = "albaranToolStripMenuItem";
-            this.albaranToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.albaranToolStripMenuItem.Text = "Albaran";
-            // 
-            // facturaToolStripMenuItem
-            // 
-            this.facturaToolStripMenuItem.Name = "facturaToolStripMenuItem";
-            this.facturaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.facturaToolStripMenuItem.Text = "Factura";
-            // 
-            // albaranToolStripMenuItem1
-            // 
-            this.albaranToolStripMenuItem1.Name = "albaranToolStripMenuItem1";
-            this.albaranToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
-            this.albaranToolStripMenuItem1.Text = "Albaran";
-            // 
-            // facturaToolStripMenuItem1
-            // 
-            this.facturaToolStripMenuItem1.Name = "facturaToolStripMenuItem1";
-            this.facturaToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
-            this.facturaToolStripMenuItem1.Text = "Factura";
-            // 
-            // albaranToolStripMenuItem2
-            // 
-            this.albaranToolStripMenuItem2.Name = "albaranToolStripMenuItem2";
-            this.albaranToolStripMenuItem2.Size = new System.Drawing.Size(152, 22);
-            this.albaranToolStripMenuItem2.Text = "Albaran";
-            // 
-            // facturaToolStripMenuItem2
-            // 
-            this.facturaToolStripMenuItem2.Name = "facturaToolStripMenuItem2";
-            this.facturaToolStripMenuItem2.Size = new System.Drawing.Size(152, 22);
-            this.facturaToolStripMenuItem2.Text = "Factura";
-            // 
-            // clientesToolStripMenuItem
-            // 
-            this.clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
-            this.clientesToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.clientesToolStripMenuItem.Text = "Clientes";
-            // 
-            // albaranesToolStripMenuItem
-            // 
-            this.albaranesToolStripMenuItem.Name = "albaranesToolStripMenuItem";
-            this.albaranesToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.albaranesToolStripMenuItem.Text = "Albaranes";
-            // 
-            // facturasToolStripMenuItem
-            // 
-            this.facturasToolStripMenuItem.Name = "facturasToolStripMenuItem";
-            this.facturasToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.facturasToolStripMenuItem.Text = "Facturas";
-            // 
-            // clientesToolStripMenuItem1
-            // 
-            this.clientesToolStripMenuItem1.Name = "clientesToolStripMenuItem1";
-            this.clientesToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
-            this.clientesToolStripMenuItem1.Text = "Clientes";
-            // 
-            // clientesToolStripMenuItem2
-            // 
-            this.clientesToolStripMenuItem2.Name = "clientesToolStripMenuItem2";
-            this.clientesToolStripMenuItem2.Size = new System.Drawing.Size(152, 22);
-            this.clientesToolStripMenuItem2.Text = "Clientes";
-            // 
-            // clientesToolStripMenuItem3
-            // 
-            this.clientesToolStripMenuItem3.Name = "clientesToolStripMenuItem3";
-            this.clientesToolStripMenuItem3.Size = new System.Drawing.Size(152, 22);
-            this.clientesToolStripMenuItem3.Text = "Clientes";
             // 
             // Form1
             // 
--- FruteriaFacturas/FruteriaFacturas/Form1.cs
+++ FruteriaFacturas/FruteriaFacturas/Form1.cs
@@ -16,5 +16,35 @@ namespace FruteriaFacturas
         {
             InitializeComponent();
         }
+
+    // CREACION DEL CLIENTE 
+        private void clientesToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            CreacionCliente formCLiente = new CreacionCliente();
+            if (!formRepetido(formCLiente))
+            {
+                this.pbfondoMDI.Visible = false;
+                formCLiente.MdiParent = this;
+                formCLiente.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+                formCLiente.Show();
+
+            }
+        }
+
+    //METODO DE VERIFICACION DE FORMULARIOS REPETIDOS
+        public bool formRepetido(Form form)
+        {
+            bool repetido = false;
+            foreach (Form frm in Application.OpenForms)
+            {
+                if (frm.Name.Equals(form.Name))
+                {
+                    repetido = true;
+                }
+            }
+            return repetido;
+        }
+
+
     }
 }
